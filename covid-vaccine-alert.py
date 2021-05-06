b20102ba63a355a91ee7bdac5bf7431868660064
Adit Negi <aditnegi@Adit-Henny.local>
2021-05-06 14:58:39 +0530

    add timeline, refactor code

--- covid-vaccine-alert.py
+++ covid-vaccine-alert.py
@@ -2,35 +2,7 @@ import requests
 import json
 import smtplib, ssl
 import time
-from datetime import datetime
-
-
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-pincode = input('input pincode you would like to search for')
-while True:
-    day = (datetime.now().day) 
-    if day< 10:
-        day = str(0)+ str(day)
-    else:
-        day = str(day)
-    month = (datetime.now().month)
-    if month<10: 
-        month =str(0)+ str(month)
-    else:
-        month = str(month)
-    year =str(datetime.now().year)
-    date = day + '-' + month + '-' + year
-    try:
-        response =requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode='+ pincode + '&date='+date, headers = headers)
-        response_json = json.loads(response.text)
-        for center in response_json['centers']:
-            for session in center['sessions']:
-                if session['available_capacity']>0 and session['min_age_limit']>=18:
-                    send_mail('Vaccine Slot Open Now', center['name'])
-    except:
-        pass
-
-    time.sleep(300)
+from datetime import datetime, timedelta
 
 def send_mail(subject, name):
     sender = 'your-email'
@@ -38,6 +10,7 @@ def send_mail(subject, name):
   
     # start TLS for security
     s.starttls()
+    print('here')
     
     # Authentication
     s.login("your-email", "your-password")
@@ -46,9 +19,46 @@ def send_mail(subject, name):
     message = subject + " "+ name
     
     # sending the mail
-    s.sendmail("your-emai", "your-password", message)
+    s.sendmail("your-email", "sender-email", message)
     
     # terminating the session
     s.quit()
+
+#Dates to start search, passed via query parameters
+def get_dates(date):
+    date2 = date
+    day2 = date2.day
+    month2 =date2.month
+    year2 = date2.year
+    if day2< 10:
+        day2 = str(0)+ str(day2)
+    else:
+        day2 = str(day2)
+    if month2<10: 
+        month2 =str(0)+ str(month2)
+    else:
+        month2 = str(month2)
+    date2 = day2+'-'+ month2+str(year2)
+    return date2
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+pincode = input('input pincode you would like to search for')
+while True:
+    #search upto 2 weeks ahead
+    date1, date2 = get_dates(datetime.now()), get_dates(datetime.now()+timedelta(days=7))
+    for date in [date1, date2]:
+        try:
+            response =requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode='+ pincode + '&date='+date, headers = headers)
+            response_json = json.loads(response.text)
+            for center in response_json['centers']:
+                for session in center['sessions']:
+                    if session['available_capacity']>0 and session['min_age_limit']==18:
+                        send_mail('Vaccine Slot Open Now', center['name'])
+        except:
+            continue
+    
+    #lets not throttle a govt site it hangs by a thread anyways
+    time.sleep(300)
+
 
 
